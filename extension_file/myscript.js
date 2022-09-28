1d632807ac517723e5b2aa309587705fba01a7d6
qsda82 <qsda82@gmail.com>
2022-09-28 16:37:06 +0800

    import complete2

--- extension_file/myscript.js
+++ extension_file/myscript.js
@@ -11,15 +11,20 @@ async function useImport() {
     //util.hello()
     // 如果是想使用export default出來的
     //util.default.hello()
-    util.default.html2canvas(document.querySelector("#captcha")).then(canvas => {
-        document.body.appendChild(canvas)
+    util.default(document.querySelector("#captcha"),{useCORS: true}).then(canvas => {
+        url=canvas.toDataURL();
+        a=document.querySelector('#a');
+        
+        a.href=url;
+        alert(a)
     });
 }
 
-chrome.runtime.onMessage.addListener(function (message, sender, callback) {
+chrome.runtime.onMessage.addListener(async function (message, sender, callback) {
     
     console.log(message, sender, callback);
     callback('我收到你的消息了：'+JSON.stringify("request"));
+    useImport()
     return true;
 });
 
